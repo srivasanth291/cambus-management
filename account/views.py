9fc8fb4d507c13989632b8cc1725f12913e996b2
srivasanth291 <srivasanth291@gmail.com>
2026-01-11 22:55:01 +0530

    Day 5:form

--- account/views.py
+++ account/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 def home(request):
     return render(request,'account/home.html')
 
 def login(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+             
     return render(request,'account/login.html')
 
 def dashboard(request):
